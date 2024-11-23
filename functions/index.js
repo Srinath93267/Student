828698664cc2f5b40650ff462dcb899fe3306724
Srinath579 <62790179+Srinath579@users.noreply.github.com>
2024-11-23 17:31:10 +0530

    deployed to google functions

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const sql = require('mssql');
 const jwt = require('jsonwebtoken'); // For generating authentication tokens
+const axios = require('axios');
 
 app.use(bodyParser.json());
 app.use(express.json());
@@ -14,6 +15,9 @@ const sqlConfig = require("./Config/studentdbazure.json");
 const API_PREFIX = '/student/';
 
 const jwtSecret = 'your_jwt_secret';
+const API_KEY = 'test';
+const STUDENT_ACADEMIC_GET_API_ENDPOINT = 'https://studentmarkdetails-dvd9d8a6gvf4hzcq.canadacentral-01.azurewebsites.net/studentAcadamics/marks/student';
+const STUDENT_ACADEMIC_DELETE_API_ENDPOINT = 'https://studentmarkdetails-dvd9d8a6gvf4hzcq.canadacentral-01.azurewebsites.net/studentAcadamics/marks';
 
 // Connect to SQL Server
 sql.connect(sqlConfig, (err) => {
@@ -91,5 +95,68 @@ app.get(API_PREFIX + 'get-all-student-details', verifyToken, async (_req, res) =
     }
 });
 
-// Export the app as a Firebase function
-exports.api = functions.https.onRequest(app);
+app.get(API_PREFIX + 'get-student-detail-and-mark-by-id/:studentid', verifyToken, async (req, res) => {
+    try {
+        const authHeader = req.headers.authorization;
+        const studentid = req.params['studentid'];
+        const result = await sql.query`EXEC GetStudentDetails @StudentID=${studentid};`;
+        const StudentAcademicData = await callApi(studentid, authHeader, STUDENT_ACADEMIC_GET_API_ENDPOINT);
+        const StudentFirstData = result.recordset
+        res.status(200).json({ StudentFirstData, StudentAcademicData }); // Send the result set as JSON
+    } catch (err) {
+        console.error('Error executing query:', err);
+        res.status(500).send('Server error');
+    }
+});
+
+app.delete(API_PREFIX + 'delete-a-student-all-detail', verifyToken, async (req, res) => {
+    try {
+        const authHeader = req.headers.authorization;
+        const { studentid } = req.body;
+        await sql.query`EXEC DeleteStudentDetailbyStudentID @StudentID=${studentid};`;
+        await callApi(studentid, authHeader, STUDENT_ACADEMIC_DELETE_API_ENDPOINT);
+        res.status(200).json("Student Detail Deleted Successfully"); // Send the result set as JSON
+    } catch (err) {
+        console.error('Error executing query:', err);
+        res.status(500).send('Server error');
+    }
+});
+
+async function callApi(studentID, authHeader, apiEndPoint) {
+    const url = `${apiEndPoint}/${studentID}`;
+    // Defining headers
+    const headers = {
+        'Authorization': `${authHeader}`,
+        'Content-Type': 'application/json',
+        'api-key': API_KEY,
+    };
+
+    try {
+        // Make the API call with headers
+        const response = await axios.get(url, {
+            headers: headers,
+        });
+        const data = response.data; // API response data
+        console.log('Response:', data);
+        // Return or store the data
+        return data;
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+const LocalDebug = true;
+
+if (LocalDebug === true) {
+    // Port Number
+    const PORT = process.env.PORT || 5000;
+
+    // Server Setup
+    app.listen(PORT, console.log(
+        `Server started on port ${PORT}`));
+}
+else {
+    // Export the app as a Firebase function
+    exports.api = functions.https.onRequest(app);
+}
